c39f4f08f62018f213486ff809376e10f3d92b1e
Rodrigo Ceccato <rodrigoceccatodefreitas@gmail.com>
2026-03-09 00:19:43 -0300

    expand Y travel for StarGraber build

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1888,7 +1888,7 @@
 #define X_MIN_POS 0
 #define X_MAX_POS 192
 #define Y_MIN_POS 8
-#define Y_MAX_POS 187
+#define Y_MAX_POS 207
 #define Z_MIN_POS 0
 #define Z_MAX_POS 150
 //#define I_MIN_POS 0
@@ -1905,8 +1905,8 @@
 //#define W_MAX_POS 50
 
 // The size of the printable area
-#define X_BED_SIZE (X_MAX_POS - X_MIN_POS)  // 192
-#define Y_BED_SIZE (Y_MAX_POS - Y_MIN_POS)  // 179
+#define X_BED_SIZE (X_MAX_POS - X_MIN_POS)
+#define Y_BED_SIZE (Y_MAX_POS - Y_MIN_POS)
 
 /**
  * Software Endstops
